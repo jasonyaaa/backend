caf6eb9c7f025cbfa59063774e9ea43782ae34c6
Sindy0514 <36184972+r0930514@users.noreply.github.com>
2025-05-28 23:58:20 +0800

    fix: 移除不必要的資料庫建立函式匯入 (#39)
    * feat: 新增必要的依賴項目；新增storage模組的初始化檔案
    
    * refactor: 重新調整專案資料夾結構
    
    * feat: 調整練習記錄表
    
    * fix: 移除不必要的註解標記
    
    * fix: 調整欄位與練習回饋相關結構
    
    * feat: 更新章節和語句相關路由
    
    * feat: 儲存服務（未完成）
    
    * refactor: 調整儲存服務位置
    
    * Chore/資料庫自動同步 (#35)
    
    * feat: 新增必要的依賴項目；新增storage模組的初始化檔案
    
    * refactor: 重新調整專案資料夾結構
    
    * feat: 調整練習記錄表
    
    * fix: 移除不必要的註解標記
    
    * fix: 調整欄位與練習回饋相關結構
    
    * feat: 更新章節和語句相關路由
    
    * feat: 儲存服務（未完成）
    
    * feat: 新增資料庫版本控制與初始化設定
    
    * feat: 移除原先自動建立表格的機制，改由完全手動控制 (#37)
    
    * feat: 新增必要的依賴項目；新增storage模組的初始化檔案
    
    * refactor: 重新調整專案資料夾結構
    
    * feat: 調整練習記錄表
    
    * fix: 移除不必要的註解標記
    
    * fix: 調整欄位與練習回饋相關結構
    
    * feat: 更新章節和語句相關路由
    
    * feat: 儲存服務（未完成）
    
    * feat: 新增資料庫版本控制與初始化設定
    
    * feat: 移除原先自動建立表格的機制，改由完全手動控制
    
    * Merge branch 'main' into chore/資料庫自動同步
    
    * fix: 移除不必要的資料庫建立函式、版本檔 (#38)
    
    * fix: 移除不必要的資料庫建立函式匯入
    
    * Merge branch 'main' into fix/main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.router import router as auth_router
 from src.course.router import router as course_router
-from src.shared.database.database import create_db_and_tables
 
 # 系統啟動時建立資料庫連線
 @asynccontextmanager
