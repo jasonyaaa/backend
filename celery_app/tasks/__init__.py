f9acf67b721273e58f54a228a607a4b685f9571b
r0930514 <r0930514@gmail.com>
2025-08-20 18:05:56 +0800

    refactor: 簡化celery_app

--- celery_app/tasks/__init__.py
+++ celery_app/tasks/__init__.py
@@ -1,7 +1,7 @@
 """
 VocalBorn Celery 任務模組
 
-包含所有的 Celery 任務定義
+包含所有的 Celery 任務定義和排程配置
 """
 
 from .analyze_audio import analyze_audio_task, AudioAnalysisError
@@ -10,11 +10,26 @@ from .cleanup_expired import cleanup_expired_tasks
 from .health_check import health_check
 from .test_task import test_task
 
+# Beat 排程配置
+CELERYBEAT_SCHEDULE = {
+    'cleanup-expired-tasks': {
+        'task': 'celery_app.tasks.cleanup_expired_tasks',
+        'schedule': 3600.0,  # 每小時執行一次
+        'options': {'queue': 'maintenance'}
+    },
+    'health-check': {
+        'task': 'celery_app.tasks.health_check',
+        'schedule': 300.0,   # 每 5 分鐘執行一次
+        'options': {'queue': 'health'}
+    },
+}
+
 __all__ = [
     "analyze_audio_task",
     "AudioAnalysisError",
     "analyze_test_audio_task",
     "cleanup_expired_tasks", 
     "health_check",
-    "test_task"
+    "test_task",
+    "CELERYBEAT_SCHEDULE"
 ]
